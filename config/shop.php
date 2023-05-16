3d716f754151371d6c301630f0ecdb66767ddc15
David Szabados <dv5150dev@gmail.com>
2023-05-16 16:11:23 +0200

    minor fixes

--- config/shop.php
+++ config/shop.php
@@ -4,6 +4,7 @@ return [
     'models' => [
         // 'user' => \App\Models\User::class,
         // 'product' => \App\Models\Shop\Product::class,
+        // 'category' => \App\Models\Shop\Category::class,
         // 'order' => \App\Models\Shop\Order::class,
         // 'orderItem' => \App\Models\Shop\OrderItem::class,
         // 'paymentMode' => \App\Models\Shop\PaymentMode::class,
