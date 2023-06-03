8922c4f363ba44bdf77f9d6b9ed2f71b5fd22ed7
David Szabados <dv5150dev@gmail.com>
2023-06-04 01:16:48 +0200

    remove comments from Cart facade

--- src/Facades/Cart.php
+++ src/Facades/Cart.php
@@ -8,25 +8,6 @@ use Illuminate\Support\Facades\Facade;
 class Cart extends Facade
 {
     /**
-     * @method static all()
-     * @method static reset()
-     *
-     * @method static addItem()
-     * @method static removeItem()
-     * @method static eraseItem()
-     *
-     * @method static setCoupon()
-     * @method static removeCoupon()
-     *
-     * @method static setShippingMode()
-     * @method static getShippingMode()
-     *
-     * @method static getSubtotal()
-     * @method static getTotal()
-     *
-     * @method static toArray()
-     * @method static toJson()
-     *
      * @see \DV5150\Shop\Services\CartService
      */
     protected static function getFacadeAccessor(): string
